554b5bc31c8f052003e00dea377bab526cb066f6
Maxime Beugnet <maxime.beugnet@gmail.com>
2018-07-26 20:31:40 +0200

    MongoDB 4.0.0 now in GA so moving for Release Candidate to stable GA version

--- start-mongo.sh
+++ start-mongo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker run --rm -d -p 27017:27017 --name mongo mongo:rc --replSet rs
+docker run --rm -d -p 27017:27017 --name mongo mongo:4.0.0 --replSet rs
 sleep 1
 docker exec -it mongo mongo --eval 'rs.initiate()'
